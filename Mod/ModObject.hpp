d309ef0550f1d178a7c895e8f2f0ec3d4c7ae6a6
AmeliaCute <48628282+AmeliaCute@users.noreply.github.com>
2025-09-04 03:30:35 +0100

    Refactor ModEnvironnement

--- Mod/ModObject.hpp
+++ Mod/ModObject.hpp
@@ -2,32 +2,34 @@
     #define MODMETA_HPP
 
     #include <string>
-    #include <vector>
+    #include <map>
+    #include <filesystem>
 
 struct ModMetaData
 {
-    std::string name;
-    std::string author;
-    std::string version;
+    std::string title;          // Some fancy display name, if someone want to do a ModMenu for configuration and stuff..
+    std::string identifier;     // The unique id of ur mod
+    std::string author;         // made by who? :3
+    float       version;        
 
-    std::vector<std::string> dependencies;
+    //      <identifier, version>
+    std::map<std::string, float> dependencies;
 };
 
 class ModObject
 {
     public:
-        ModObject(ModMetaData meta, std::filesystem::path origin) :
-        
-        metadata(meta),
-        path(origin) 
-        {};
+        ModObject(ModMetaData meta, std::filesystem::path container, std::string internalPath = {}):
+        _metaData(std::move(meta)), _containerPath(std::move(container)), _internalPath(std::move(internalPath)) {}
 
-        ModMetaData             getMeta() { return metadata; }
-        std::filesystem::path   GetPath() { return path; }
+        ModMetaData           getMeta()             { return _metaData; };
+        std::filesystem::path getContainerPath()    { return _containerPath; }
+        std::string           getInternalPath()     { return _internalPath; }
 
     private:
-        ModMetaData metadata;
-        std::filesystem::path path;
+        ModMetaData             _metaData;
+        std::filesystem::path   _containerPath;
+        std::string             _internalPath;
 };
 
 #endif //! MODMETA_HPP

a6c98f27169b3f68441bd881f001735835f150f2
AmeliaCute <48628282+AmeliaCute@users.noreply.github.com>
2025-12-18 11:56:20 +0000

    EXPERIMENTAL: add GameRegistries

--- API/Registry/RegistryBase.hpp
+++ API/Registry/RegistryBase.hpp
@@ -4,16 +4,15 @@
   #include <string>
   #include <unordered_map>
 
-
 template<typename T>
 class IRegistry 
 {
     public:
     virtual ~IRegistry() = default;
     
-    virtual T* get(const std::string& key) = 0;
-    virtual bool contains(const std::string& key) const = 0;
-    virtual size_t size() const = 0;
+    virtual T* Get(const std::string& key) = 0;
+    virtual bool Contains(const std::string& key) const = 0;
+    virtual size_t Size() const = 0;
     
     virtual std::unordered_map<std::string, uint32_t>::const_iterator begin() const = 0;
     virtual std::unordered_map<std::string, uint32_t>::const_iterator end() const = 0;
